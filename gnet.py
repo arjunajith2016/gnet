092ea25ca4e2caf105dca03bff564f38f2bd40eb
Arjun <arjun.ajith@grofers.com>
2016-01-25 14:22:22 +0530

    Heroku trial #5

--- gnet.py
+++ gnet.py
@@ -130,4 +130,6 @@ def not_found(error):
 
 # to start the server
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
